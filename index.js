f00d5921c882dfd59d450ec858484023970b7a4d
Oleg <www.runit@gmail.com>
2023-01-20 12:13:49 +0300

    add db & dotenv

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import router from "./router.js";
+import { Sequelize } from 'sequelize';
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 
-const PORT = 5000
-const db = `mongodb+srv://user:22042204Q@cluster0.qwnsk8j.mongodb.net/?retryWrites=true&w=majority`
+const PORT = process.env.SERVER_PORT
+// const db = `mongodb+srv://user:22042204Q@cluster0.qwnsk8j.mongodb.net/?retryWrites=true&w=majority`
+const sequelize = new Sequelize(process.env.CONNECTION_BD)
 
 const app = express()
 
@@ -12,14 +16,16 @@ app.use(express.json())
 app.use('/api', router)
 
 app.get('/', (req, res) => {
-    res.status(200).json('Server Page')
+    res.status(200).json('Server work')
 })
 
 async function startApp() {
     try {
-        await mongoose.set('strictQuery', false)
-        await mongoose.connect(db)
-        app.listen(PORT, () => console.log('Server work ' + PORT))
+        // await mongoose.set('strictQuery', false)
+        // await mongoose.connect(db)
+        // app.listen(PORT, () => console.log('Server work ' + PORT))
+        await sequelize.authenticate()
+        app.listen(PORT, () => console.log('Connection seccessfull'))
     } catch (e) {
         console.log(e)
     }
