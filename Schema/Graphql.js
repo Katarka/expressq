1075437e7e0981b093f6f476e6e9e95333d1f884
Katarka <www.runit@gmail.com>
2023-02-06 20:49:40 +0300

    add schema graphl for gallery

--- Schema/Graphql.js
+++ Schema/Graphql.js
@@ -1,16 +1,23 @@
 import {ApolloServer, gql} from "apollo-server-express";
 import Post from "../Model/Post.js";
+import Gallery from "../Model/Gallery.js";
 
 const typeDefs = gql`
     type Query {
         Posts: [Post]
         Post(id: ID!): Post
+        Galleries: [Gallery]
+        Gallery(id: ID!): Gallery
     }
     type Post {
         author: String!,
         title: String!,
         content: String!,
         picture: String
+    }
+    type Gallery {
+        name: String!,
+        picture: String!
     }
 `
 
@@ -21,6 +28,14 @@ const resolvers = {
         },
         Post(_, {id}){
             return Post.findOne({
+                where: { id: id }
+            })
+        },
+        Galleries() {
+            return Gallery.findAll()
+        },
+        Gallery(_, {id}){
+            return Gallery.findOne({
                 where: { id: id }
             })
         }
