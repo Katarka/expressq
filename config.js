ce7c01111936592fd85d99db63fb3a555a8db932
Oleg <www.runit@gmail.com>
2023-03-06 15:33:46 +0300

    add CORS

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const smtp = {
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    user: process.env.MAIL_USER,
-    from: process.env.MAIL_FROM,
-    to: process.env.MAIL_TO,
-    token: process.env.MAIL_TOKEN
+    host: 'smtp.gmail.com',
+    port: 465,
+    user: 'sendmailerdoors@gmail.com',
+    from: 'sendmailerdoors@gmail.com',
+    to: 'duka@live.ru',
+    token: 'ya29.a0AVvZVsqLKj9Q4qVMDl---hdx88la-52xzaJHyR5m48nmBoPPzv5ZDW8E5a4qNaJ-2kJvKxpoYCpcqXFpAAdc-GLGQP1F_cP_7NJI4HRsEqwVLMUT72e49mnrE6yWv0g-30P1eaEw6sTh6SySziW1Np-kHo3paCgYKAc8SARESFQGbdwaI0PYBxef8hAc1Ig4V7NTdtA0163'
 }
 
 export default smtp
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-// import mongoose from 'mongoose'
 import routerPost from "./router/routerPost.js";
 import * as dotenv from 'dotenv'
 import Post from './Model/Post.js';
@@ -11,12 +10,21 @@ import routerGallery from "./router/routerGallery.js";
 import bodyParser from 'body-parser'
 import mailer from './nodemailer.js'
 import smtp from './config.js';
+import cors from 'cors'
 
 dotenv.config()
 
 const PORT = process.env.SERVER_PORT
-let order = undefined
-// const db = process.env.MONGO
+const whitelist = ['http://127.0.0.1:5173']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 const app = express()
 
@@ -26,13 +34,9 @@ app.use(express.static('static/gallery'))
 app.use(fileupload({}))
 app.use('/api', routerPost, routerGallery)
 
-app.get('/', (req, res) => {
-    res.status(200).json('Server work')
-})
-
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.post('/feedback', (req, res) => {
+app.post('/feedback', cors(corsOptions), (req, res, next) => {
     if(!req.body.name || !req.body.phone) return res.status(400)
     const message = {
         to: smtp.to,
@@ -43,20 +47,15 @@ app.post('/feedback', (req, res) => {
         <b>Телефон:</b> ${req.body.phone}`
     }
     mailer(message)
-    order = req.body
     res.redirect(301, 'http://127.0.0.1:5173/')
 })
 
-// app.get('/feedback', (req, res) => {
-//     if(typeof order !== 'object') return res.redirect(301, 'http://127.0.0.1:5173/')
-//     order = undefined
-// })
+app.get('/', (req, res) => {
+    res.status(200).json('Server work')
+})
 
 async function startApp() {
     try {
-        // await mongoose.set('strictQuery', false)
-        // await mongoose.connect(db)
-        // app.listen(PORT, () => console.log('Server work ' + PORT))
         await sequelize.authenticate()
         await Post.sync()
         await Gallery.sync()
